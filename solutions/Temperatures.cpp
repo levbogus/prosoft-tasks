92a51c13a6f949e83271b1011f3b8b8ecd0f24ac
levbogus <lev.bog12@gmail.com>
2025-09-29 20:23:55 +0500

    Update Temperatures.cpp

--- solutions/Temperatures.cpp
+++ solutions/Temperatures.cpp
@@ -18,7 +18,7 @@ int main()
     for (int i = 0; i < n; i++) {
         int t; // a temperature expressed as an integer ranging from -273 to 5526
         cin >> t; cin.ignore();
-        if(abs(t)-abs(answer)<0||(t+answer==0&&t>=0)) answer = t;
+        if(abs(t)<abs(answer)||(t+answer==0&&t>=0)) answer = t;
     }
     if(n==0) answer=0;
     // Write an answer using cout. DON'T FORGET THE "<< endl"
