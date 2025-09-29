12354824fe6e08dbcf1bdcdf75543ca5bab6e349
levbogus <lev.bog12@gmail.com>
2025-09-29 13:37:24 +0500

    Update The_Descent.cpp

--- solutions/The_Descent.cpp
+++ solutions/The_Descent.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+
+int main()
+{
+
+    // game loop
+    while (1) {
+        int MAX = -1;
+        int target = 0;
+        for (int i = 0; i < 8; i++) {
+            int mountain_h; // represents the height of one mountain.
+            cin >> mountain_h; cin.ignore();
+            if(mountain_h>MAX){
+                MAX = mountain_h;
+                target = i;
+            }
+        }
+        
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        cout << target << endl; // The index of the mountain to fire on.
+    }
+}
